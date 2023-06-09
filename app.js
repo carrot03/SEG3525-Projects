4c9f6ea64b11f1a7575ccfd601fe9cd7b596a387
[Hiba] <htant016@uottawa.ca>
2023-06-08 21:31:35 -0400

    feat: finished the  base project

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function showMore(){
-    var x = document.getElementById("showingSerives");
+function showMore(hiddenBlock){
+    var x = document.getElementById(hiddenBlock);
     if (x.style.display === "none") {
       x.style.display = "flex";
     } else {
@@ -41,11 +41,16 @@ function bookingSubmit() {
     fullName = document.getElementById('username')
     email = document.getElementById('userEmail')
     pro = document.getElementById('selectedPro')
+    time = document.getElementById('timeInput')
+    date = document.getElementById('dateInput')
+    service = document.getElementById('selectedService')
+    pass = document.getElementById('Password')
 
-     window.alert("Thank you " + fullName.value + " for booking with " + pro.value + "... A confirmation Email has been sent to: " + email.value)
-    
+    if (validate(email, pro, time, date, service, fullName, pass)) {
+        window.alert("Thank you " + fullName.value + " for booking with " + pro.value + " at " + time.value + " on " + date.value  + "... A confirmation Email has been sent to: " + email.value)
+    }
     //validate time input
-    var timeInput = document.getElementById("timeInput");
+    /*var timeInput = document.getElementById("timeInput");
     var dateInput = document.getElementById("dateInput");
     var timeValue = timeInput.value;
     var dateInput = dateInput.value;
@@ -58,9 +63,52 @@ function bookingSubmit() {
       timeInput.setCustomValidity(""); // Time is within schedule
     } else {
       timeInput.setCustomValidity("Please enter a time between 08:00 and 18:00"); // Time is outside schedule
-    }
+    }*/
+
+    //modal display
+    /*modal = document.getElementById('confirmationModal')
+    modalText = document.getElementById('modaltext')
+    modalText.value = "Thank you " + fullName.value + " for booking with " + pro.value + "... A confirmation Email has been sent to: " + email.value
+    modal.style.display = "modal" */
+    
   }
 
   function goto($hashtag){
     document.location = "index.html#" + $hashtag;
 }
+
+function validate(email, pro, time, date, service, fullname, pass){
+    if (email.value == "") {
+      alert("L'espace d'Email doit être remplie");
+      return false;
+    }
+    if (pro.value == "") {
+        alert("Un coiffeur doit etre choisi");
+        return false;
+      }
+    if (time.value == "") {
+        alert("Un temps doit etre choisi");
+        return false;
+    }
+    if (date.value == "") {
+        alert("Une date doit etre choisie");
+        return false;
+    }
+    if (service.value == "") {
+        alert("Une service doit etre choisie");
+        return false;
+    }
+    if (fullname.value == "") {
+        alert("L'espace de Nom et Prenom doit etre remplie");
+        return false;
+    }
+    
+    if (pass.value == "") {
+        alert("L'espace du mot de passe doit etre remplie");
+        return false;
+    }
+    
+    return true;
+
+}
+
